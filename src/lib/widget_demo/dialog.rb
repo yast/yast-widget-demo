ae09ef817b9394b5532a0ca5c0a64e8753712272
Stefan Hundhammer <shundhammer@suse.de>
2021-05-12 17:31:29 +0200

    Use wizard dialog

--- src/lib/widget_demo/dialog.rb
+++ src/lib/widget_demo/dialog.rb
@@ -19,6 +19,7 @@
 require "yast"
 
 Yast.import "UI"
+Yast.import "Wizard"
 
 module Yast
   module WidgetDemo
@@ -28,12 +29,14 @@ module Yast
       include Yast::Logger
 
       def initialize
-        # to do
+        @next_step_id = "step_00"
       end
 
       # Displays the dialog
       def run
-        return unless create_dialog
+        create_dialog
+        show_page(page1)
+        set_wizard_steps
 
         begin
           return event_loop
@@ -45,29 +48,67 @@ module Yast
       private
 
       def create_dialog
-        # Intentionally no translation in this module
-        UI.OpenDialog(
-          MinSize(
-            50, 10,
-            HVCenter(
-              VBox(
-                Label("Hello, world!"),
-                VSpacing(1),
-                PushButton(Id(:ok), Opt(:default), "&OK")
-              )
-            )
-          )
-        )
+        Wizard.OpenNextBackStepsDialog
       end
 
       def close_dialog
         UI.CloseDialog
       end
 
+      def show_page(page_contents)
+        title = "Wizard Dialog"
+        has_back = false
+        has_next = true
+        Wizard.SetContents(title, page_contents, help_text, has_back, has_next)
+      end
+
+      def page1
+        # Intentionally no translations in this module
+        Label("Hello, world!")
+      end
+
+      def help_text
+        # Intentionally no translations in this module
+        "Help text"
+      end
+
+      def set_wizard_steps
+        delete_wizard_steps
+        add_wizard_step_heading("Widget Demo")
+        add_wizard_step("Simple Widgets")
+        add_wizard_step("Selection Widgets")
+        add_wizard_step("Table")
+        add_wizard_step("Tree")
+        add_wizard_step_heading("Package Management")
+        add_wizard_step("PatternSelector")
+        add_wizard_step("PackageSelector")
+        mark_wizard_step("step_01")
+      end
+
+      def delete_wizard_steps
+        UI.WizardCommand(Yast.term(:DeleteSteps))
+        @next_step_id = "step_00"
+      end
+
+      def add_wizard_step_heading(text)
+        # This is safe to use even with NCurses: It does nothing
+        UI.WizardCommand(Yast.term(:AddStepHeading, text))
+      end
+
+      def add_wizard_step(text, step_id = nil)
+        # Steps without an ID are ignored
+        step_id ||= @next_step_id.next!
+        UI.WizardCommand(Yast.term(:AddStep, text, step_id))
+      end
+
+      def mark_wizard_step(step_id)
+        UI.WizardCommand(Yast.term(:SetCurrentStep, step_id))
+      end
+
       def event_loop
         loop do
           case input = UI.UserInput
-          when :ok, :cancel # :cancel is WM_CLOSE
+          when :ok, :next, :abort, :cancel # :cancel is WM_CLOSE
             # Break the loop
             break
           else
